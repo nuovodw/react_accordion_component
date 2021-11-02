77b5893db55e71af6d3d55f2387bc47bed7eec04
Daniel Won <dan.j.won@gmail.com>
2021-11-02 06:52:51 -0500

    create an array and map over accordion component

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Accordion = () => {
-	return (
-		<div>
-			<h1>Accordion</h1>
-		</div>
-	);
+const Accordion = ({ items }) => {
+	const renderedItems = items.map((item) => {
+		return (
+			<div key={item.title}>
+				<div className="title active">
+					<i className="drowdown icon"></i>
+					{item.title}
+				</div>
+				<div className="content active">
+					<p>{item.content}</p>
+				</div>
+			</div>
+		);
+	});
+
+	return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Accordion from './Accordion';
 
+const items = [
+	{
+		title: 'What is your profession?',
+		content: 'I am a front end developer.',
+	},
+	{
+		title: 'What is your tech stack?',
+		content: 'React, Javascript, Material UI',
+	},
+	{
+		title: 'What do you like about being a dev?',
+		content: 'I love solving problems and creating apps and websites.',
+	},
+];
+
 export default () => {
 	return (
 		<div>
-			<Accordion />
+			<Accordion items={items} />
 		</div>
 	);
 };
