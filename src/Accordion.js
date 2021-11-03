076480b53906ab1cd25b7a52410368bae8362db8
Daniel Won <dan.j.won@gmail.com>
2021-11-03 04:41:43 -0500

    add onclick

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
-	const renderedItems = items.map((item) => {
+	const onTitleClick = (index) => {
+		return console.log('Title Clicked', index);
+	};
+
+	const renderedItems = items.map((item, index) => {
 		return (
-			<div key={item.title}>
-				<div className="title active">
-					<i className="drowdown icon"></i>
+			<React.Fragment key={item.title}>
+				<div className="title active" onClick={() => onTitleClick(index)}>
+					<i className="dropdown icon"></i>
 					{item.title}
 				</div>
 				<div className="content active">
 					<p>{item.content}</p>
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	});
 
